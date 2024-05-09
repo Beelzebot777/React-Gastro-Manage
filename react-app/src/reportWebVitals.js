2491ad449679b920e77ffa4aed42976945c69076
Gabriel Roca <blast.blackstone@gmail.com>
2024-05-09 21:13:34 +0000

    Install React and Tailwind

---
+++ react-app/src/reportWebVitals.js
@@ -0,0 +1,15 @@
+// reportWebVitals.js is a file that is used to report web vitals, which are metrics that measure user experience on a website.
+
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry); // Cumulative Layout Shift
+      getFID(onPerfEntry); // First Input Delay
+      getFCP(onPerfEntry); // First Contentful Paint
+      getLCP(onPerfEntry); // Largest Contentful Paint
+      getTTFB(onPerfEntry); // Time to First Byte
+    });
+  }
+};
+
+export default reportWebVitals;
